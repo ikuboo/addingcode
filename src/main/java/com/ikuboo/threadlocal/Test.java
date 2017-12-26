d96c4fe6a6ae71550dd322a0591047bbc8b54d92
yuanchunsen <yuanchunsen@jd.com>
2017-12-26 21:55:45 +0800

    1

---
+++ src/main/java/com/ikuboo/threadlocal/Test.java
@@ -0,0 +1,11 @@
+package com.ikuboo.threadlocal;
+
+/**
+ * @author yuanchunsen@jd.com
+ *         2017/12/26.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}

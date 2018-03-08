1d5a190a0085ff48901ec29f04d12db0a3036199
ikuboo <ikuboo@126.com>
2018-03-09 00:04:39 +0800

    测试jstack命令

--- src/main/java/com/ikuboo/multithread/base/jstack/TestDeadLock.java
+++ src/main/java/com/ikuboo/multithread/base/jstack/TestDeadLock.java
@@ -1,24 +1,56 @@
 package com.ikuboo.multithread.base.jstack;
 
 /**
- * 测试 jstack 命令
+ * 用 jstack 命令 分析死锁
  *  jstack pid
  * Created by Administrator on 2018/3/8 0008.
  */
-public class TestJstack {
+public class TestDeadLock {
     public static void main(String[] args) throws InterruptedException {
-        Object o = new Object();
+        //deadLock方法会出发死锁
+        //t1 thead 持有A锁等待B锁，t2 线程持有B锁等A锁，故会造成死锁
+        TestDeadLock.deadLock();
 
-        final Thread t = new Thread(new Runnable() {
+    }
+
+    private static String A = "A";
+    private static String B = "B";
+
+    private static void deadLock(){
+        Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                while (true){}
+                synchronized (A){
+                    try {
+                        Thread.sleep(2000);
+                        synchronized (B){
+                            System.out.println("t1");
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-        }, "test_jstack_commond_thread");
-        t.start();
+        });
 
-        synchronized (o){
-            o.wait();
-        }
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (B){
+                   try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (A){
+                        System.out.println("t2");
+                    }
+                }
+            }
+        });
+
+
+        t1.start();
+        t2.start();
     }
 }

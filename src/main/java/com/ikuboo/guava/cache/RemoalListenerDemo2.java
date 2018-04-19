d72422de48f403696c442136e2b03ee42dc9bcdf
yuanchunsen <yuanchunsen@jd.com>
2018-04-19 16:33:05 +0800

    guava cache 过期监听器demo2

--- src/main/java/com/ikuboo/guava/cache/RemoalListenerDemo2.java
+++ src/main/java/com/ikuboo/guava/cache/RemoalListenerDemo2.java
@@ -2,17 +2,13 @@ package com.ikuboo.guava.cache;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
 import com.google.common.cache.RemovalListener;
 import com.google.common.cache.RemovalListeners;
 import com.google.common.cache.RemovalNotification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.RejectedExecutionHandler;
@@ -20,56 +16,63 @@ import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author yuanchunsen@jd.com
- *         2018/1/21.
+ * 2018/1/21.
  */
 public class RemoalListenerDemo2 {
     private static final Logger logger = LoggerFactory.getLogger(RemoalListenerDemo2.class);
 
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
+    public static void main(String[] args) {
 
 
-        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(10,10,100, TimeUnit.SECONDS,
+        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(10, 10, 100, TimeUnit.SECONDS,
                 new LinkedBlockingQueue<Runnable>(100000),
                 new ThreadFactory() {
                     private AtomicInteger counter = new AtomicInteger(0);
+
                     public Thread newThread(Runnable r) {
-                        return new Thread(r, "guava-cache-thread-" + counter.getAndIncrement());
+                        return new Thread(r, "guava-cache-threads-" + counter.getAndIncrement());
                     }
                 },
 
                 new RejectedExecutionHandler() {
                     public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
-                        logger.warn("Task {} rejected from {}",r.toString(), executor.toString());
+                        logger.warn("Task {} rejected from {}", r.toString(), executor.toString());
                     }
                 }
         );
 
 
-        Cache<String, String> cache = CacheBuilder.newBuilder()
+        Cache<String, AtomicLong> guavaCache = CacheBuilder.newBuilder()
                 .expireAfterWrite(1, TimeUnit.SECONDS)
                 .initialCapacity(1000)
                 .maximumSize(1000)
                 .removalListener(
-                        RemovalListeners.asynchronous(new RemovalListener<String, String>() {
-                            public void onRemoval(RemovalNotification<String, String> notification) {
-                                   logger.info(notification.getValue());
+                        RemovalListeners.asynchronous(new RemovalListener<String, AtomicLong>() {
+                            public void onRemoval(RemovalNotification<String, AtomicLong> notification) {
+                                logger.info(String.format("threadName=%s,value=%s", Thread.currentThread().getName(), notification.getValue().get()));
                             }
                         }, threadPool)
                 )
                 .build();
 
+        while (true) {
+            try {
+                AtomicLong key = guavaCache.get("key", new Callable<AtomicLong>() {
+                    @Override
+                    public AtomicLong call() throws Exception {
+                        return new AtomicLong(0);
+                    }
+                });
 
-        Long value = 0L;
-        for (;;){
-            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss,SSS");
-            String nowStr = sdf.format(new Date());
-            cache.put("key:" + nowStr, nowStr);
-            TimeUnit.SECONDS.sleep(3);
+                key.incrementAndGet();
+
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
         }
-
-
     }
 }

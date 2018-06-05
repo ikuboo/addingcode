5957846b06bac12b41b97bef278988ef456c2462
yuanchunsen <IKUBOOikuboo@121>
2018-06-05 21:37:17 +0800

    打印斐不那契数列

--- src/main/java/com/ikuboo/algorithm/chapter1/Exercises1_1_6.java
+++ src/main/java/com/ikuboo/algorithm/chapter1/Exercises1_1_6.java
@@ -1,7 +1,7 @@
 package com.ikuboo.algorithm.chapter1;
 
 /**
- * 第一章练习题1.1.5
+ * 第一章练习题1.1.6
  */
 public class Exercises1_1_6 {
     public static void main(String[] args) {
@@ -12,5 +12,25 @@ public class Exercises1_1_6 {
             f = f + g;
             g = f - g;
         }
+
+        System.out.println("--------------------");
+
+        print();
+
     }
+
+
+    public static void print(){
+        int n1 = 0;
+        int n2 = 1;
+
+        for (int i = 0; i < 10; i++) {
+            int temp = n2;
+            n2 = n2 + n1;
+            n1 = temp;
+            System.out.println(n2);
+
+        }
+    }
+
 }

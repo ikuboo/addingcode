cce19b4c0024b0fd7427e171459560095683c3ff
yuanchunsen <yuanchunsen@jd.com>
2018-06-17 16:51:56 +0800

    算法  练习题

---
+++ src/main/java/com/ikuboo/algorithm/chapter1/Exercises1_1_9.java
@@ -0,0 +1,12 @@
+package com.ikuboo.algorithm.chapter1;
+
+/**
+ * 第一章练习题1.1.8
+ */
+public class Exercises1_1_9 {
+    public static void main(String[] args) {
+
+        String s = Integer.toBinaryString(100);
+        System.out.println(s);
+    }
+}

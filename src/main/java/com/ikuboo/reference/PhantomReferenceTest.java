6bbccf2f6cd18715fb71e28a675e2d820e9af466
元春森 <yuanchunsen@meituan.com>
2018-08-06 10:26:54 +0800

    java 软引用 弱引用 虚引用 demo

---
+++ src/main/java/com/ikuboo/reference/PhantomReferenceTest.java
@@ -0,0 +1,26 @@
+package com.ikuboo.reference;
+
+import java.lang.ref.WeakReference;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 弱引用Demo
+ */
+public class WeakReferenceTest {
+    public static void main(String[] args) throws InterruptedException {
+        //new 一个String对象
+        String str = new String("Hello,everyone");//①
+        //创建一个软引用指向到字符串"Hello,everyony"
+        WeakReference<String> wr = new WeakReference<>(str);//②
+        //切断str 与 "Hello,everyony" 的关联
+        str = null;//③
+
+        System.out.println("GC前:" + wr.get());
+        //强制执行GC
+        System.gc();
+        System.runFinalization();
+        //因为GC的不确定性，所以GC后睡眠一会
+        TimeUnit.SECONDS.sleep(5);
+        System.out.println("GC前:" + wr.get());
+    }
+}
---
+++ src/main/java/com/ikuboo/reference/SoftReferenceTest.java
@@ -0,0 +1,4 @@
+package com.ikuboo.reference;
+
+public class SoftReferenceTest {
+}

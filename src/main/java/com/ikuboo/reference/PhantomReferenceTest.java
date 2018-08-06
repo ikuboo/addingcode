0fe71db286bc71c422b5233d86fa8209fc6202fe
元春森 <yuanchunsen@meituan.com>
2018-08-06 10:27:27 +0800

    java 软引用 弱引用 虚引用 demo

--- src/main/java/com/ikuboo/reference/PhantomReferenceTest.java
+++ src/main/java/com/ikuboo/reference/PhantomReferenceTest.java
@@ -1,26 +1,36 @@
 package com.ikuboo.reference;
 
-import java.lang.ref.WeakReference;
+import java.lang.ref.PhantomReference;
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
 import java.util.concurrent.TimeUnit;
 
 /**
- * 弱引用Demo
+ * 虚引用Demo
  */
-public class WeakReferenceTest {
+public class PhantomReferenceTest {
     public static void main(String[] args) throws InterruptedException {
         //new 一个String对象
-        String str = new String("Hello,everyone");//①
-        //创建一个软引用指向到字符串"Hello,everyony"
-        WeakReference<String> wr = new WeakReference<>(str);//②
-        //切断str 与 "Hello,everyony" 的关联
-        str = null;//③
+        String str = new String("Hello,everyone");
+        //创建一个引用队列
+        ReferenceQueue<String> rq = new ReferenceQueue<>();
+        //创建一个虚引用指向到字符串"Hello,everyony"
+        PhantomReference pr = new PhantomReference(str, rq);
 
-        System.out.println("GC前:" + wr.get());
+        //不能通过虚引用访问所引用对象，所以会打印null
+        System.out.println("通过虚引用获取对象为:" + pr.get());//①
+
+        //切断str 与 "Hello,everyony" 的关联
+        str = null;
+
         //强制执行GC
         System.gc();
         System.runFinalization();
         //因为GC的不确定性，所以GC后睡眠一会
         TimeUnit.SECONDS.sleep(5);
-        System.out.println("GC前:" + wr.get());
+
+        //取出引用队列中最先放入队列中的PhantomReference引用与pr对比
+        Reference<? extends String> poll = rq.poll();
+        System.out.println("" + (poll == pr));//②
     }
 }
--- src/main/java/com/ikuboo/reference/SoftReferenceTest.java
+++ src/main/java/com/ikuboo/reference/SoftReferenceTest.java
@@ -1,4 +1,56 @@
 package com.ikuboo.reference;
 
+import java.lang.ref.SoftReference;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 软引用Demo
+ */
 public class SoftReferenceTest {
+    static volatile boolean runFlag = true;
+    public static void main(String[] args) throws InterruptedException {
+        //new 一个String对象
+        String str = new String("Hello,everyone");//①
+        //创建一个软引用指向到字符串"Hello,everyony"
+        SoftReference<String> sr = new SoftReference<>(str);//②
+        //切断str 与 "Hello,everyony" 的关联
+        str = null;//③
+
+        //启动一个线程，不停的消耗内存
+        Thread t = new Thread(new Runnable() {
+            List<byte[]> eatMemList = new LinkedList<>();
+            @Override
+            public void run() {
+                while (true){
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                        eatMemList.add(new byte[1024 * 1024]);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }catch (OutOfMemoryError error){
+                        System.out.println("内存不足了..");
+                        runFlag = false;
+                        break;
+                    }
+                }
+            }
+        });
+        t.start();
+
+        while (runFlag){
+            //强制执行GC
+            System.gc();
+            System.runFinalization();
+
+            //通过软引用获取字符串,null 表示已经别被回收
+            String s = sr.get();//④
+            if(null == s){
+                System.out.println("字符串被垃圾回收器回收掉了");
+            }else {
+                System.out.println(s);
+            }
+        }
+    }
 }
